ad8f25a0abe337e068fc21660944255e1cb8f2cb
Aleksey Fedorov <pioniro@yandex.ru>
2017-11-25 16:50:01 +0300

    bug: Prevent case, when i18next tag has no children

--- src/component.js
+++ src/component.js
@@ -29,6 +29,8 @@ export default {
         if (match.length === 0) return memo;
 
         child = match;
+      } else if (!children) {
+        return memo;
       } else {
         const place = match.trim();
         if (isNaN(parseFloat(place)) || !isFinite(place)) {
